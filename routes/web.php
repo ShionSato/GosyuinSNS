bb84f54ca28578a86a49bcf542cf83d88d43b9b2
ShionSato <208449@st.takushoku-u.ac.jp>
2023-05-04 11:29:15 +0900

    blog

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PostController;  //外部にあるPostControllerクラスをインポート。
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,10 @@ use App\Http\Controllers\PostController;  //外部にあるPostControllerクラ�
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/posts', [PostController::class, 'index']); 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class, 'index']); 
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::post('/posts', [PostController::class, 'store']);
+Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
+Route::put('/posts/{post}', [PostController::class, 'update']);
+Route::delete('/posts/{post}', [PostController::class,'delete']);
+Route::get('/posts/{post}', [PostController::class ,'show']);
